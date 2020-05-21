ebc29c397448711fdcfccff77eb0e694a82f54d5
dyte <dieter.walckiers@gmail.com>
2020-05-21 13:02:00 +0200

    wip

---
+++ src/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+      {
+      name: 'netlify',
+      options: {
+        title: 'Netlify deploys',
+        sites: [
+          {
+            title: 'La Barraca website',
+            apiId: 'b20a5190-a462-4b04-aa16-c201aebb9472',
+            buildHookId: '5ec65889e09ff5211934a869',
+            name: 'sanity-labarraca-web'
+          }
+        ]
+      }
+    }
+  ]
+}
