7b584476e91a02b2080b2e79c52ff2490a3c00b7
Dieter Walckiers <dieter@manual.to>
2022-01-25 21:09:21 +0100

    temp disable sent mails

--- plugins/transactionalMail/TransactionalMail.js
+++ plugins/transactionalMail/TransactionalMail.js
@@ -17,59 +17,60 @@ import gql from "graphql-tag";
 import { useQuery } from "@apollo/client";
 import ApolloClientProvider from "../shared/ApolloClientProvider";
 
-const TransactionalMail = () => {
+// const TransactionalMail = () => {
 
-    const [cursorBefore, setCursorBefore] = useState(undefined);
-    const [cursorAfter, setCursorAfter] = useState(undefined);
-    const [mailSentEvents, setMailSentEvents] = useState([]);
+//     const [cursorBefore, setCursorBefore] = useState(undefined);
+//     const [cursorAfter, setCursorAfter] = useState(undefined);
+//     const [mailSentEvents, setMailSentEvents] = useState([]);
 
-    const { data } = useQuery(gql`
-        {
-            allMailSentEvents(_size: 20) {
-                data {
-                    type
-                    to
-                    created
-                    wasAccepted
-                    wasRejected
-                    response
-                }
-                before
-                after
-            }
-        }
-    `);
+//     const { data } = useQuery(gql`
+//         {
+//             allMailSentEvents(_size: 20) {
+//                 data {
+//                     type
+//                     to
+//                     created
+//                     wasAccepted
+//                     wasRejected
+//                     response
+//                 }
+//                 before
+//                 after
+//             }
+//         }
+//     `);
 
-    const allMailSentEvents = data?.allMailSentEvents;
-    useEffect(() => {
-        if (allMailSentEvents?.data) {
-            setMailSentEvents(allMailSentEvents.data);
-        }
-        if (allMailSentEvents?.before) {
-            setCursorBefore(allMailSentEvents?.before);
-        }
-        if (allMailSentEvents?.after) {
-            setCursorAfter(allMailSentEvents?.after);
-        }
-    }, [allMailSentEvents]);
+//     const allMailSentEvents = data?.allMailSentEvents;
+//     useEffect(() => {
+//         if (allMailSentEvents?.data) {
+//             setMailSentEvents(allMailSentEvents.data);
+//         }
+//         if (allMailSentEvents?.before) {
+//             setCursorBefore(allMailSentEvents?.before);
+//         }
+//         if (allMailSentEvents?.after) {
+//             setCursorAfter(allMailSentEvents?.after);
+//         }
+//     }, [allMailSentEvents]);
 
-    console.log(`cursorBefore ${cursorBefore}`)
-    console.log(`cursorAfter ${cursorAfter}`)
-    console.log(`mailSentEvents ${mailSentEvents}`)
+//     console.log(`cursorBefore ${cursorBefore}`)
+//     console.log(`cursorAfter ${cursorAfter}`)
+//     console.log(`mailSentEvents ${mailSentEvents}`)
 
-    return (
-        <div className={styles.TransactionalMail}>
-            <TransactionalMailTable mailSentEvents={mailSentEvents} />
-        </div>
+//     return (
+//         <div className={styles.TransactionalMail}>
+//             <TransactionalMailTable mailSentEvents={mailSentEvents} />
+//         </div>
 
-    );
-};
+//     );
+// };
 
 const TransactionalMailWrapper = (props) => {
     return (
-        <ApolloClientProvider>
-            <TransactionalMail {...props} />
-        </ApolloClientProvider>
+        // <ApolloClientProvider>
+        //     <TransactionalMail {...props} />
+        // </ApolloClientProvider>
+        <span>coming soon!</span>
     );
 };
 
